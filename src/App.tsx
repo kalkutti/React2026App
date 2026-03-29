8abb6752572a0b52a3cf883e916ac6e1afeeab7b
kalkutti <kalindu.sembakutti@gmail.com>
2026-03-29 19:24:09 -0400

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/header.tsx'
 import Home from './components/home.tsx'
 import { Container } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
 
-function App() {
+const App = () => {
+  const dresses = useSelector( (state) => state.dresses.list)
 
   return (
     <Container className="py-4">
       <Header/>
       <Home/>
+      <p>Dresses</p>
+      <div>
+        { dresses ?
+          dresses.map( dress => (
+            <p key={dress.id}>{dress.title}</p>
+          ))
+        :null}
+      </div>
     </Container>
   )
 }
--- src/store/dress.ts
+++ src/store/dress.ts
@@ -2,10 +2,14 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const dressSlice = createSlice({
     name: 'dresses',
-    initialState:{},
-    reducers:{
-
+    initialState:{
+        list:[
+            { id: 1, title: 'Coat' },
+            { id: 2, title: 'Pants' },
+            { id: 3, title: 'Shirt' },
+        ]
     },
+    reducers:{},
 })
 
 export default dressSlice.reducer
